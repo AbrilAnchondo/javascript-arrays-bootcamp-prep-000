6ef1f5555144496e18c947c081a0345cbf3e4061
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-07-24 18:58:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,4 +21,7 @@ function addElementToBeginningOfArray (array, element) {
   function accessElementInArray (array, index){
     return array[index];
   }
-  
+  function destructivelyRemoveElementFromBeginningOfArray (array){
+    return array.shift();
+    
+  }
